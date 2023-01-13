ec59fcbb31c4ab995cacd67939e6ff4489d03637
Jose Paez <josefinapaezdevalle@gmail.com>
2023-01-13 14:26:44 -0300

    ProyectoFinal

---
+++ src/components/data/stock.js
@@ -0,0 +1,68 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Remera zafari",
+        tipo: "Remera",
+        desc: "¡Conseguila en todos los talles!",
+        precio: 5000,
+        talle: "Del 6 al 14",
+        img: '/public/images/cae3132b895693f0b2c50bebf5c84f52761303dad779b507b143085eafd6720268929.jpeg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Campera de Papel",
+        tipo: "Campera",
+        desc: "¡Ideal para desplegar la creatividad y dibujar!",
+        precio: 15000,
+        talle: "Del 8-18",
+        img: '/public/images/ff8085a9b5297fa8fedc53c88fde28f571de33b5704021923c1369f24664c34968929.png',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Jardinero Rosa",
+        tipo: "Jardinero",
+        desc: "¡Conseguilo en todos los talles!",
+        precio: 7000,
+        talle: "6-18",
+        img: '/public/images/descarga.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Campera canchera",
+        tipo: "Campera",
+        desc: "campera con estampa en mangas. Conseguila en todos los talles",
+        precio: 10000,
+        talle: "8-16",
+        img: '/public/images/camperaconestampaenmangas.jpeg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Campera Diversamente",
+        tipo: "Campera",
+        desc: "Conseguila en todos los talles, del 8 al 16. Para leer estampa en lengua de señas",
+        precio: 5500,
+        talle: "8-16",
+        img: '/public/images/camperadiversamente.jpeg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Campera de lluvia Reversible",
+        tipo: "Campera",
+        desc: "campera reversible. Conseguila en todos los talles, del 8 al 16.",
+        precio: 500,
+        talle: "8-16",
+        img: '/public/images/camperadelluviareversible.png',
+        cantidad:1
+    }
+];
